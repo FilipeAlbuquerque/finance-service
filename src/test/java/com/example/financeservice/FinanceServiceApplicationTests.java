58aca788fc91e521113452e92b113c2c9f6eab69
FilipeAlbuquerque <filipefragosoti@gmail.com>
2025-03-29 21:44:34 +0000

    feat: add github workflow actions

--- src/test/java/com/example/financeservice/FinanceServiceApplicationTests.java
+++ src/test/java/com/example/financeservice/FinanceServiceApplicationTests.java
@@ -1,24 +1,17 @@
 package com.example.financeservice;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+
+@DataJpaTest
 @ActiveProfiles("test")
 class FinanceServiceApplicationTests {
 
-  // Specify bean name to avoid ambiguity
-  @MockBean(name = "userDetailsService")
-  private org.springframework.security.core.userdetails.UserDetailsService userDetailsService;
-
-  @MockBean
-  private AuthenticationManager authenticationManager;
-
   @Test
   void contextLoads() {
+    // This just verifies that the test context loads successfully
     System.out.println("Context loaded successfully.");
   }
 }
